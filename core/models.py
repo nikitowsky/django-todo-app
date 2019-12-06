dac43f3d10d72f0b14acd9b4870c47a4ad4d24fb
Nikita Borisowsky <yuzirock@gmail.com>
2019-12-06 16:29:56 +0200

    Add todo model

---
+++ core/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class BaseModel(models.Model):
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super(BaseModel, self).save(*args, **kwargs)
+
+
+class Tag(BaseModel):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+class Todo(BaseModel):
+    title = models.CharField(max_length=255)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return self.title
---
+++ core/tests/test_models.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+
+from core.models import Todo
+
+
+class ModelsTestCase(TestCase):
+    def test_create_todo_without_title(self):
+        """
+        Cannot create a todo item without name,
+        should throw an validation error
+        """
+
+        todo = Todo()
+
+        self.assertRaises(ValidationError, todo.save)
